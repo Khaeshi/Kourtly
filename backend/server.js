a09eb073df0b7903516c16e9a7db2cd8e8ac8f52
Khaesey Angel Tablante <kagtabss@gmail.com>
2026-03-12 20:51:53 +0800

    kwjdnskensn

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,8 @@ import userRoutes from './src/routes/userRoutes.js';
 import playerRoutes from './src/routes/playerRoutes.js';
 import queueRoutes from './src/routes/queueRoutes.js';
 import itemRoutes from './src/routes/itemRoutes.js';
-import tabRoutes  from './src/routes/tabRoutes.js';
-import reservation from './src/routes/reservationRoutes.js'
-
+import tabRoutes from './src/routes/tabRoutes.js';
+import reservation from './src/routes/reservationRoutes.js';
 
 dotenv.config();
 
@@ -16,26 +15,26 @@ const app = express();
 
 app.use(cors({
   origin: [
-    'http://localhost:3000',
-    'https://your-app.vercel.app'  
-  ],
-  credentials: true
-}));
+      'http://localhost:3000',
+      'https://badminton-scbc.vercel.app',
+      ],
+credentials: true
+    }));
 app.use(express.json());
 
-// MongoDB connection
-mongoose.set('returnDocument', 'after')
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+              // MongoDB connection
+              mongoose.set('returnDocument', 'after')
+              mongoose.connect(process.env.MONGODB_URI)
+                .then(() => console.log('MongoDB connected'))
+                  .catch(err => console.error(err));
 
-// Routes
-app.use('/api/users', userRoutes);
-app.use('/api/players', playerRoutes);
-app.use('/api/queue', queueRoutes);
-app.use('/api/items', itemRoutes);
-app.use('/api/tabs',  tabRoutes);
-app.use('/api/reservations', reservation)
+                  // Routes
+                  app.use('/api/users',        userRoutes);
+                  app.use('/api/players',      playerRoutes);
+                  app.use('/api/queue',        queueRoutes);
+                  app.use('/api/items',        itemRoutes);
+                  app.use('/api/tabs',         tabRoutes);
+                  app.use('/api/reservations', reservation);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+                  const PORT = process.env.PORT || 5000;
+                  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
